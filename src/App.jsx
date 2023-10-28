806c3b05256c4383cd364bd3e36409a9d79240b9
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2023-10-28 16:26:35 +0300

    Feedback component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import css from './App.module.scss';
+import React, { Component } from 'react';
 import Feedback from './components/Feedback';
+import css from './App.module.scss';
 
-export const App = () => {
-  return (
-    <div className={css.bodyWrapper}>
-      <h2>Feedback widget</h2>
-      <Feedback />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedback = feedback => {
+    this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
+  };
+
+  render() {
+    return (
+      <div className={css.bodyWrapper}>
+        <h2>Feedback widget</h2>
+        <Feedback options={this.state} onClick={this.addFeedback} />
+      </div>
+    );
+  }
+}
